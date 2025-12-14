f449455856c4ce1e7875ef5540d5db97453639a8
Jacob <jacobulysseslockwood@gmail.com>
2025-12-14 12:15:19 -0500

    idk

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Jacob Lockwood : WJS-LPA
 
-g=(d,f)=>f?d&&f(d):d
+let g=(d,f)=>f?d&&f(d):d,X=e=>{throw""+e},
 prs=s=>{let bdg={},fs=""
-,r=a=>new RegExp("^"+a.raw).exec(s=s.replace(/^ +/,x=>(fs+=" ","")))
+,r=a=>new RegExp("^"+a.raw).exec(s=s.replace(/^[ ;]+/,
+  x=>(x.includes(" ")?fs+=" ":0,"")))
 ,ts=_=>{_=fs;r``;fs=_}
 ,c=p=>f=>g(r(p),m=>(s=s.slice(m[0].length),f?f(...m):m[0]))
 ,j=p=>f=>c(p)((...m)=>(fs+=m[0],f?f(...m):m[0]))
@@ -31,67 +32,83 @@ prs=s=>{let bdg={},fs=""
    :p[0]!=3?[a,x==2?[3,n,p]:x?[n,p]:X`unexpected nilad`]
    :[x>a?x:a,[p[1],p[2],x?[n,[1],x>1&&[2]]:n]]),[0])
   return e[1][0]!=3?e:[2,[e[1][1],e[1][2],[2]]]}
-,pgm=[],out=[],lines=[],prvs=s
- while(s){j`\s*(NB\..*)?\s*`();pgm.push([l(),fs.trim()]);fs=""
+,pgm=[],out=[],prvs=s,x
+ while(s){x=l();j`#.+`();pgm.push([x,fs]);j`\n`();fs=""
   if(s==prvs)throw console.error("bad token @",s);prvs=s}
  for(let n in bdg)if(bdg[n][1][0]=="E")bdg[n]=rd(bdg[n])
  for(let[x,fs]of pgm)
-  out.push([fs,x[0]=="E"?rd(x):["B",x[1],bdg[x[1]]]])
+  out.push([fs,!x?0:x[0]=="E"?rd(x):["B",x[1],bdg[x[1]]]])
  return out}
 
 let bdg={}
-,mrg=a=>a.some(v=>!mch(v[0],a[1][0]))?X
-  :[[a.length,...a[0][0]],a.flatMap(v=>v[1])]
+,A=(d,s=[d.length])=>(d.s=s,d),S=d=>A([d],[]),R=a=>a.s.length
+,mrg=a=>a.some(v=>!mch(v.s,a[0].s))?X`mrg shapes must match`
+  :A(a.flat(),[a.length,...a[0].s])
 ,ev=(n,arg,[a,b,c]=n,r=v=>ev(v,arg))=>!b?arg[a-1]
   :a=="B"?bdg[b]=ev(c):b=="R"?bdg[c]
   :b[0]=="S"?mrg(n.slice(2).map(r))
-  :b=="T"?[[c.length],[...c]]:b=="N"||b=="C"?[[],[c]]
+  :b=="T"?A([...c]):b=="N"||b=="C"?S(c)
   :b[0]>2?fns[b[1]][2](b[2][0],b[3]?.[0])[1](ev(b[2]),g(b[3],ev))
   :a.map?r(a)(r(b),g(c,r))
   :b.map?a?(...s)=>ev(b,s):ev(b)
   :fns[b][2]
-,pty=([s,d])=>s.length*t(d[0])==1?`"${d.join``}"`
+,pty=(d,s=d.s)=>s.length*t(d[0])==1?`"${d.join``}"`
   :d.slice(0,s[0]).map((l,i)=>s.length<2?t(l)?l:l<0?"¯"+-l:l
-   :`(${pty([s.slice(1),d.slice(i*(l=d.length/s[0]),++i*l)])})`
+   :`(${pty(A(d.slice(i*(l=d.length/s[0]),++i*l),s.slice(1)))})`
   ).join` `
 ,mch=(a,b)=>a.length==b.length&&a.every((v,i)=>v===b[i])
-,rws=([s,d])=>d.slice(0,s[0]).map((l,i)=>
-  [s.slice(1),d.slice(i*(l=d.length/(s[0]??1)),++i*l)])
+,rws=(d,[n,...r]=d.s)=>d.slice(0,n).map((l,i)=>
+  A(d.slice(i*(l=d.length/(n??1)),++i*l),r))
 ,pfm=(a,b,l=Math.min(a.length,b.length))=>
   mch(a.slice(0,l),b.slice(0,l))
-,prv=f=>(x,y,
-  dx=x[1],dy=y?.[1],sx=x[0],sy=y?.[0],rx=sx.length,ry=sy?.length
- )=>!y?[sx,dx.map(v=>f(v))]
-  :mch(sx,sy)?[sx,dx.map((v,i)=>f(v,dy[i]))]
-  :!pfm(sx,sy)?X
-  :rx<ry?[sy,dy.map((v,i)=>f(dx[i*dx.length/dy.length|0],v))]
-        :[sx,dx.map((v,i)=>f(v,dy[i*dy.length/dx.length|0]))]
-,fns={},r={},[mf,df,mm,dm]=[1,2,3,4].map(n=>
-  (_,s=_.raw[0],g=s[0],m=s.slice(2))=>f=>{
-   r[m]=r[g]=f;fns[m]=fns[g]=[n,g,f]})
+,prv=f=>(x,y)=>!y?A(x.map(v=>f(v)),x.s)
+  :mch(x.s,y.s)?A(x.map((v,i)=>f(v,y[i])),x.s)
+  :!pfm(x.s,y.s)?X`prv shape prefixes must match`
+  :R(x)<R(y)?A(y.map((v,i)=>f(x[i*x.length/y.length|0],v)),y.s)
+            :A(x.map((v,i)=>f(v,y[i*y.length/x.length|0])),x.s)
+,fns={},r={}
+,[mf,df,mm,dm]=[1,2,3,4].map(n=>
+  (_,s=_.raw[0],g=s[0],m=s.slice(2))=>x=>(
+   fns[m]=fns[g]=[n,g,
+    r[m]=r[g]=(...a)=>(_=X,X=s=>_(g+" ! "+s),a=x(...a),X=_,a)]
+   ))
+// ,mf=(_)
+// mf`g name`(x)
+// f=(a,v)=>{v=X;X=s=>v()}
+,prd=s=>s.reduce((x,y)=>x*y,1)
 ,[pm,pd]=[mf,df].map(g=>s=>f=>g(s)(prv(f)))
-,t=v=>["number","string","object"].indexOf(
-  typeof(Array.isArray(v)?v[1][0]:v))
+,t=v=>["number","string","object"].indexOf(typeof(v.s?v[0]:v))
 ,ch=String.fromCodePoint,od=c=>c.codePointAt(0)
 ,L=(...x)=>console.log(...x)
 
-pd`+ add`((x,y,s=t(x)+t(y))=>s==1?ch(t(x)?od(x)+y:od(y)+x):s?X:x+y)
+mf`∘ id`(x=>x);df`⊣ lft`(x=>x);df`⊢ rgt`((x,y)=>y)
+mf`, rav`(x=>A([...x]))
+mf`△ sha`(x=>A(x.s))
+mf`⧻ len`(x=>S(x.s[0]??1))
+
+pm`⌵ abs`(Math.abs)
+pm`± sig`(Math.sign)
+pd`+ add`((x,y,s=t(x)+t(y))=>
+ s==1?ch(t(x)?od(x)+y:od(y)+x):!s?x+y:X`x<-n or y<-n`)
 pd`- sub`((x,y,tx=t(x),ty=t(y))=>
- tx?ty?od(x)-od(y):ch(od(x)-y):ty?X:x-y)
-pd`× mul`((x,y)=>x*y)
-pd`÷ div`((x,y)=>x/y)
-df`⊂ joi`((x,y)=>[[x[0][0]+y[0][0]],x[1].concat(y[1])])
-df`↯ res`((x,y)=>[y[1],x[1]])
+ tx?ty?od(x)-od(y):ch(od(x)-y):!ty?x-y:X`y<-n if x<-n`)
+pd`× mul`((x,y)=>t(x)+t(y)?X`x<-y<-n`:x*y)
+pd`÷ div`((x,y)=>t(x)+t(y)?X`x<-y<-n`:x/y)
+
+df`↯ res`((x,y,l=prd(y))=>
+ A(Array(Math.ceil(l/x.length)).fill(x).flat().slice(0,l),y))
 mf`⇌ rev`(x=>mrg(rws(x).reverse()))
-mf`⧻ len`(x=>[[],[x[0][1]]])
-mf`⇡ ran`(x=>x[0].length?X:[x[1],[...Array(x[1][0]).keys()]])
+// df`⊂ joi`((x,y)=>x+y)
+// rank-generic join
+mf`⇡ ran`(x=>R(x)?X`x<-sc`:A([...Array(x[0]).keys()]))
 mm`˙ slf`(a=>[1,f=>    x=>a?f(x,x):f])
 mm`˜ bac`(a=>[2,f=>(x,y)=>a?f(y,x):f])
-mm`/ red`(a=>a<2?X:[1,f=>x=>rws(x).reduce((a,b)=>f(a,b))])
-mm`\ sca`(a=>a<2?X:[1,f=>x=>mrg(rws(x).map((a,i)=>x=i?f(x,a):a))])
-dm`⟜ aft`((r,l)=>!l||l+r<2?X
- :[r>1?2:1,(b,a)=>(x,y,v=r?b(x,y):b)=>l>1?a(x,v):a(v)])
+mm`/ red`(a=>a<2?X`f<-dy`:[1,f=>x=>rws(x).reduce((a,b)=>f(a,b))])
+mm`\ sca`(a=>a<2?X`f<-dy`
+ :[1,f=>x=>mrg(rws(x).map((a,i)=>x=i?f(x,a):a))])
+dm`⟜ aft`((r,l)=>!l||l+r<2?X`g<-`
+ :[r>1?2:1,(f,g)=>(x,y,v=r?f(x,y):f)=>l>1?g(x,v):g(v)])
 //dm`⊸ bef`
 
 for(let[s,p]of prs(require("fs").readFileSync(0)+"")){
- L("    "+s);l=ev(p);L(typeof l=="function"?"fn":pty(l))}
+ L("    "+s);p&&L(typeof(p=ev(p))=="function"?"fn":pty(p))}
